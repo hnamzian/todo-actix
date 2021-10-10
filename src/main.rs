4d16783f7b65e6a63ee9505ddc79af82852a53d4
hnamzian <namazian87@gmail.com>
2021-10-10 19:16:22 +0300

    Create GET /status endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use actix_web::{HttpServer, App};
+use actix_web::{HttpServer, App, Responder, web};
+
+async fn status() -> impl Responder {
+    "{\"status\": \"UP\"}"
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
+            .route("/", web::get().to(status))
     })
     .bind("127.0.0.1:8080")?
     .run()
